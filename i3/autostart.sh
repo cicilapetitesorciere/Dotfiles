c7b429de934dabc590ed78af8590b8d8c1862aae
cicilapetitesorcere <cgtneely@uwaterloo.ca>
2023-03-28 08:34:49 -0400

    slight modifications to unclutter

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 picom -b
 ~/.config/i3/bg.sh
-unclutter --timeout 0.2 --exclude-root &
+unclutter --timeout 0.2 &
 dunst &
 batsignal -b -D "systemctl suspend" -C "notify-send Battery Critical" -W "notify-send Battery Warning" &
 xss-lock -- ~/.config/i3/lock.sh &
