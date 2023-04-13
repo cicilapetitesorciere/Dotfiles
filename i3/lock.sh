4cc057b83bb44e85dddb4132a56cfe3c1e7da995
cicilapetitesorcere <cgtneely@uwaterloo.ca>
2023-04-13 16:27:15 -0400

    Documents now sync automatically

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+kitty sh -c "echo Syning Documents... && cd ~/Documents && git pull && git add . &&  git commit -m \"...\" && git status && git push"
 i3lock -eup win -i ~/.config/i3/wallpapers/windows95.png
