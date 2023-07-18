5a7e30063c6c2ac19e1a846532896495c70f9324
Ciaran Neely <cgtneely@uwaterloo.ca>
2023-07-18 19:18:53 -0400

    Got rid of stretch on lockscreen

--- sway/lock.sh
+++ sway/lock.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 kitty sh -c "echo Syning Documents... && cd ~/Documents && git pull && git add . &&  git commit -m \"...\" && git status && git push"
-swaylock -u -i "~/Dotfiles/sway/lockscreen.jpg" -s stretch
+swaylock -u -i "~/Dotfiles/sway/lockscreen.jpg"
