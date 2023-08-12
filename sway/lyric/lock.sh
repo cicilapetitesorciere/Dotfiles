e2e61827bced4543bd6fdb8ae0fe1470717b2679
Ciaran Neely <cgtneely@uwaterloo.ca>
2023-08-11 21:17:00 -0400

    Bug fix with locking

--- sway/lyric/lock.sh
+++ sway/lyric/lock.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 kitty sh -c "echo Syning Documents... && cd ~/Documents && git pull && git add . &&  git commit -m \"...\" && git status && git push"
-
-# swaylock -u -i "~/Dotfiles/sway/lockscreen.jpg"
-# swaylock -u -i "~/Dotfiles/sway/lockscreen.png" -s stretch
+swaylock -u -i "~/.config/sway/lockscreen.jpg"
