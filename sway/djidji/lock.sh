6fcb8d0b78c534ab25f3f53266dc9bd5126dd432
Ciaran Neely <cgtneely@uwaterloo.ca>
2025-01-02 14:58:45 -0500

    Only syncs archive

--- sway/djidji/lock.sh
+++ sway/djidji/lock.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-kitty sh -c "echo Syning Documents... && cd ~/Documents && git pull && git add . &&  git commit -m \"...\" && git status && git push"
+kitty sh -c "echo Syning Archive... && cd ~/Documents/archive && git pull && git add . &&  git commit -m \"...\" && git status && git push"
 swaylock -u -i "~/.config/sway/lockscreen.png" -s stretch
